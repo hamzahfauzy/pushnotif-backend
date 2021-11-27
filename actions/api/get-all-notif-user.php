9fae555ef66d9be1601bce17f1b0b32f6aaed1c0
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-11-27 18:08:49 +0700

    add tgl indo

--- actions/api/get-all-notif-user.php
+++ actions/api/get-all-notif-user.php
@@ -6,5 +6,11 @@ $db    = new Database($conn);
 $query = "SELECT * FROM notifications WHERE user_id=$_GET[user_id] or user_id is NULL ORDER BY sent_at DESC";
 $db->query = $query;
 $notifications = $db->exec('all');
+foreach($notifications as $notif)
+{
+    $tanggal = date('Y-m-d',strtotime($notif->sent_at));
+    $waktu = date('H:i',strtotime($notif->sent_at));
+    $notif->tanggal = tgl_indo($tanggal) .' '.$waktu;
+}
 echo json_encode($notifications);
 die();
