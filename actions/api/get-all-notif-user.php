52c9b0c47a3cd77eb4f8d13dbdee8eecb92cf448
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-11-27 18:04:04 +0700

    update order by sent at

--- actions/api/get-all-notif-user.php
+++ actions/api/get-all-notif-user.php
@@ -3,7 +3,7 @@
 $conn  = conn();
 $db    = new Database($conn);
 
-$query = "SELECT * FROM notifications WHERE user_id=$_GET[user_id] or user_id is NULL";
+$query = "SELECT * FROM notifications WHERE user_id=$_GET[user_id] or user_id is NULL ORDER BY sent_at DESC";
 $db->query = $query;
 $notifications = $db->exec('all');
 echo json_encode($notifications);
